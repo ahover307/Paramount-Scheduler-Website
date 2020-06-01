94f07ef9fea0974bc4e35caa2e34cc35ebf83fd0
Alex H <ahover307@gmail.com>
2020-06-01 12:04:12 -0400

    Begin update of app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+
+import PartyDescriptionPage from "./components/info/PartyDescriptionPage";
+import ParentScheduler from "./components/parentSchedulingProcess/ParentScheduler";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+
+            <Switch>
+                <Route exact path={'/'}> <LandingPage/> </Route>
+                <Route path={'/description'}> <PartyDescriptionPage/> </Route>
+                <Route path={'/scheduler'}> <ParentScheduler/> </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
